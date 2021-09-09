9848ac3651be29f065c746ae6ab35a015c6f059c
Krrupa <sudonthineni@gmail.com@users.noreply.github.com>
2021-09-09 17:41:28 +0530

    added README.MD

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# auth-to-bigquery
+## Installation
+
+- [Installation](#installation)
+  - [Pre-requisites](#pre-requisites)
+    - [Docker](#docker)
+    - [Pre-commit](#pre-commit)
+  - [Installation steps](#installation-steps)
+
+### Pre-requisites
+## Pre-commit
+Plio utilizes the power of pre-commit to identify simple programming issues at the time of code check-in. This helps the reviewer to focus more on architectural and conceptual issues and reduce the overall time to market.
+
+The pre-commit configurations are stored in [.pre-commit-config.yaml](../.pre-commit-config.yaml) file.
+
+To know about the syntax, visit the [official documentation site](https://pre-commit.com/).
+
+The pre-commit hooks in this repository are 
+    - flake8:  flake8 is a command-line utility for enforcing style consistency across Python projects.
+    - autopep8: autopep8 automatically formats Python code to conform to the PEP 8 style guide.
+
+1. Install pre-commit
+    Use `pip` to install pre-commit
+    ```sh
+    pip install pre-commit
+    ```
+
+    Or using homebrew on macOS
+    ```sh
+    brew install pre-commit
+    ```
+
+    For more installation alternatives, check out [Pre-commit official documentation](https://pre-commit.com/#install).
+2. Verify pre-commit installation
+    ```sh
+    pre-commit --version
--- lambda_function.py
+++ lambda_function.py
@@ -3,44 +3,47 @@ import os
 import logging
 from google.cloud import bigquery
 
-def lambda_handler(event, context):
+
+def lambda_handler(event):
     messageBody = event["Records"]
 
     for eachMessage in messageBody:
 
-        #extracting the body of the message
+        # extracting the body of the message
         message = json.loads(eachMessage["Sns"]["Message"])[0]
-        
+
         row = {}
-        
-        #the key values are the column names in the BQ table. 
+
+        # the key values are the column names in the BQ table.
         row["timestamp"] = message["dateTime"]
         row["purpose_type"] = message["purpose"]["type"]
         row["purpose_sub_type"] = message["purpose"]["subType"]
-        row["platform"] = message["purpose"]["params"]["platform"] 
-        row["platform_id"] = message["purpose"]["params"]["id"] 
-        row["auth_type"] = message["authType"] 
-        row["user_id"] = message["user"]["values"] 
+        row["platform"] = message["purpose"]["params"]["platform"]
+        row["platform_id"] = message["purpose"]["params"]["id"]
+        row["auth_type"] = message["authType"]
+        row["user_id"] = message["user"]["values"]
         row["user_data_validated"] = message["user"]["userDataValidated"]
 
         return insert_data(row)
 
+
 def insert_data(row):
 
     # load env variables
-    project_id = os.environ.get('BIGQUERY_PROJECT_ID')
-    dataset_id = os.environ.get('BIGQUERY_DATASET_ID')
-    table_id = os.environ.get('TABLE_ID')
+    project_id = os.environ.get("BIGQUERY_PROJECT_ID")
+    dataset_id = os.environ.get("BIGQUERY_DATASET_ID")
+    table_id = os.environ.get("TABLE_ID")
 
     client = bigquery.Client(project=project_id)
     table_ref = client.dataset(dataset_id).table(table_id)
     table = client.get_table(table_ref)
 
-    #Inserts data into a table
+    # Inserts data into a table
     errors = client.insert_rows_json(table, [row])
     if errors == []:
         logging.info("New rows have been added")
         return {"statusCode": 200, "body": "All done!"}
     else:
-        logging.error("Encountered errors while inserting rows: {}".format(errors))
+        logging.error(
+            "Encountered errors while inserting rows: {}".format(errors))
         return {"statusCode": 500, "body": "Error in adding row!"}
